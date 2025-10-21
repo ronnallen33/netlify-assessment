ac4c7e28e4db6dc6455e02c832a92bf304d117e8
Ronn Tolentino <ronnallentolentino@gmail.com>
2025-10-21 10:37:09 +0800

    Add _redirect and Hello js

---
+++ functions/hello.js
@@ -0,0 +1,7 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: "Hello from Netlify Functions!" })
+  };
+};
